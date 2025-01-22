1a21b5f9f22f7285dce5d9b37f0e023b5877e26e
Faith <73722237+faithofspades@users.noreply.github.com>
2025-01-22 13:07:19 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+Goals for the day:
+    1. Review and make objects with nested properties
+    2. Become familiar with Data Types in javascript
+    3. Use String Literals to easily format text strings
+ */
+
